b9f19d0d5baa8ec709b0929db611dcc46c4e2855
poyen0806 <leo33917888@gmail.com>
2024-04-15 04:11:47 +0800

    refactor: split the function into sign up and sign in

--- lib/view_model/user_view_model.dart
+++ lib/view_model/user_view_model.dart
@@ -5,17 +5,44 @@ import 'package:pre_assessment/repo/user_repo.dart';
 class UserViewModel with ChangeNotifier {
   User? _user;
   get user => _user;
+  String? _error;
+  get error => _error;
+
+  Future<bool> signUp(String email, String password) async {
+    _user = User(
+      email: email,
+      password: password,
+    );
+    if (await UserRepo.createUser(_user!) != null) {
+      notifyListeners();
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
 
   Future<bool> signIn(String email, String password) async {
     _user = User(
       email: email,
       password: password,
     );
-
-    await UserRepo.createUser(_user!);
-    notifyListeners();
-
-    return true;
+    try {
+      if (await UserRepo.getUser(_user!.email, _user!.password) != null) {
+        notifyListeners();
+        return true;
+      }
+      else {
+        throw "User not found";
+      }
+    }
+    catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+      _error = e.toString();
+      return false;
+    }
   }
 
   Future<void> signOut() async {
